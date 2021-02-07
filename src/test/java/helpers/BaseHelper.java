16ede5d3f2f3795354471676ebd2b8a2049f19fd
iamdusan <dusnen@gmail.com>
2021-02-07 17:19:43 +0100

    initial commit

---
+++ src/test/java/helpers/BaseHelper.java
@@ -0,0 +1,21 @@
+package helpers;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BaseHelper
+{
+    protected static WebDriver driver = new ChromeDriver();
+    protected static WebDriverWait wdWait = new WebDriverWait(driver, 10);
+
+    public static WebElement returnElementAttValue(String attributeName, String attributeValue)
+    {
+        String selector = "["+ attributeName + "=" + attributeValue +"]";
+        WebElement element = driver.findElement(By.cssSelector(selector));
+        return  element;
+    }
+}
